d8c5a72004d65f9ad7df369ba1630cd619503caa
Zef <tsumirez@zoho.com>
2024-02-26 07:59:27 +0100

    added a directive to link - v-bind. anchor won't work otherwise.
    v-bind:href=data_value
    
    but it can be short-handed to just the colon

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,7 @@
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
       </div>
     </div>
   </header>
@@ -13,7 +13,8 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'Zef Career Finder'
+      company: 'Zef Career Finder',
+      url: 'https://careers.google.com'
     }
   }
 }
