a5796532605804ec8aafc4c560ebf63b21fe3393
Zef <tsumirez@zoho.com>
2024-02-26 08:51:37 +0100

    added and styled links to MainNav component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,6 +3,24 @@
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full w-full list-none">
+            <li class="h-full"><a href=" " class="flex h-full items-center py-5">Teams</a></li>
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-5">Location</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-5">Life with us</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-5">How we Hire</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-5">Students</a>
+            </li>
+            <li class="ml-9 h-full"><a href="" class="flex h-full items-center py-5">Jobs</a></li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
