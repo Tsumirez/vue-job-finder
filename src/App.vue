24c17df1ee6c5b9cc0431103d4da24e1fc73d207
Zef <tsumirez@zoho.com>
2024-03-03 17:12:31 +0100

    Created HeroPanel component and added it to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <MainNav />
+  <HeroPanel />
 </template>
 
 <script>
 import MainNav from '@/components/MainNav.vue'
+import HeroPanel from '@/components/HeroPanel.vue'
 
 export default {
   name: 'App',
   components: {
-    MainNav
+    MainNav,
+    HeroPanel
   }
 }
 </script>
---
+++ src/components/HeroPanel.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <section>text</section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'HeroPanel'
+}
+</script>
