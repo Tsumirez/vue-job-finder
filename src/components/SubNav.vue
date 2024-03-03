4b67fb2d229c00512aa082069416f16cfc1cca92
Zef <tsumirez@zoho.com>
2024-03-03 08:14:04 +0100

    added search icon to SubNav

--- src/components/SubNav.vue
+++ src/components/SubNav.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
-    <div class="flex h-full items-center px-8">Sub Nav</div>
+    <div class="flex h-full items-center px-8">
+      <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
+      <span><span class="text-brand-green-1">864</span> jobs matched</span>
+    </div>
   </div>
 </template>
 
