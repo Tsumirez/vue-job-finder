cd6120983e3cabf5f2f5d5434518d161e8895db7
Zef <tsumirez@zoho.com>
2024-03-03 07:32:00 +0100

    Styled the SubNav Component and added font awesome packages into the project

--- src/components/SubNav.vue
+++ src/components/SubNav.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Sub nav</div>
+  <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
+    <div class="flex h-full items-center px-8">Sub Nav</div>
+  </div>
 </template>
 
 <script>
