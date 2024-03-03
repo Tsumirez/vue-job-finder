13e45c90dfafae103d479418cef73bc836eab47f
Zef <tsumirez@zoho.com>
2024-03-03 20:29:43 +0100

    Added css grid layout for Hero Panel

--- src/components/HeroPanel.vue
+++ src/components/HeroPanel.vue
@@ -1,6 +1,13 @@
 <template>
   <main class="flex h-screen flex-col bg-yellow-200 pb-20 pt-10">
-    <section>text</section>
+    <section>
+      <div class="grid h-screen grid-cols-12">
+        <div class="col-span-1 col-start-1">1 column</div>
+        <div class="col-span-5">5 columns</div>
+        <div class="col-span-5">5 columns</div>
+        <div class="col-span-1">1 column</div>
+      </div>
+    </section>
   </main>
 </template>
 
