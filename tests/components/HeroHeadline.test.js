52a178efdbce8144d51d0e166d58348e4f675df1
Zef <tsumirez@zoho.com>
2024-03-06 00:04:49 +0100

    Added test for HeroHealine automatic catchphrase change after passage of certain time.

--- tests/components/HeroHeadline.test.js
+++ tests/components/HeroHeadline.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/vue'
 import HeroHeadline from '@/components/HeroHeadline.vue'
 import { expect } from 'vitest'
+import { nextTick } from 'vue'
 
 describe('The HeroHeadline', () => {
   it('Displays introductory action verb', () => {
@@ -22,6 +23,21 @@ describe('The HeroHeadline', () => {
     render(HeroHeadline)
 
     expect(mock).toHaveBeenCalled()
+    vi.useRealTimers()
+  })
+
+  it('swaps action verb after interval', async () => {
+    vi.useFakeTimers()
+    render(HeroHeadline)
+    vi.advanceTimersToNextTimer()
+
+    await nextTick()
+
+    const actionPhrase = screen.getByRole('heading', {
+      name: /Build and get paid for your genius/i
+    })
+
+    expect(actionPhrase).toBeInTheDocument()
     vi.useRealTimers()
   })
 })
