42ce7b0152c92e7975b79952207e5646fdf4da25
Zef <tsumirez@zoho.com>
2024-03-07 11:08:21 +0100

    Refactored the tests for HeroHeadline with forEach and afterEach logic

--- tests/components/HeroHeadline.test.js
+++ tests/components/HeroHeadline.test.js
@@ -1,11 +1,18 @@
 import { render, screen } from '@testing-library/vue'
 import HeroHeadline from '@/components/HeroHeadline.vue'
-import { expect } from 'vitest'
+import { beforeEach, expect } from 'vitest'
 import { nextTick } from 'vue'
 
 describe('The HeroHeadline', () => {
-  it('Displays introductory action verb', () => {
+  beforeEach(() => {
     vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('Displays introductory action verb', () => {
     render(HeroHeadline)
 
     const actionPhrase = screen.getByRole('heading', {
@@ -13,21 +20,17 @@ describe('The HeroHeadline', () => {
     })
 
     expect(actionPhrase).toBeInTheDocument()
-    vi.useRealTimers()
   })
 
   it('changes action verb at intervals', () => {
-    vi.useFakeTimers()
     const mock = vi.fn()
     vi.stubGlobal('setInterval', mock)
     render(HeroHeadline)
 
     expect(mock).toHaveBeenCalled()
-    vi.useRealTimers()
   })
 
   it('swaps action verb after interval', async () => {
-    vi.useFakeTimers()
     render(HeroHeadline)
     vi.advanceTimersToNextTimer()
 
@@ -38,18 +41,15 @@ describe('The HeroHeadline', () => {
     })
 
     expect(actionPhrase).toBeInTheDocument()
-    vi.useRealTimers()
   })
 
   it('removes interval when component is destroyed', () => {
-    vi.useFakeTimers()
     const clearInterval = vi.fn()
     vi.stubGlobal('clearInterval', clearInterval)
 
     const { unmount } = render(HeroHeadline)
     unmount()
     expect(clearInterval).toHaveBeenCalled()
-    vi.useRealTimers()
     vi.unstubAllGlobals()
   })
 })
